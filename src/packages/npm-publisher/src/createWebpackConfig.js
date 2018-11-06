5e3ae6228ac7fd64bace40f0143b45a55564ad7d
Martin Zlámal <mrtnzlml@gmail.com>
2018-11-06 15:48:49 +0100

    Publisher: implement 'publish' feature
    This package now publishes registered packages automatically on NPM. Next steps are:
    
    - verify that the published packages are usable
    - write README docs
    - make it nice, maybe tests
    
    kiwicom-source-id: 5760687ac248e0100a86af610cc516dd3530ac6b

---
+++ src/packages/npm-publisher/src/createWebpackConfig.js
@@ -0,0 +1,53 @@
+// @flow
+
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+import paths from '../../../../paths';
+
+export default function createWebpackConfig(
+  packageFolder: string,
+  packageJSON: Object,
+) {
+  const packageMainFile = packageJSON.main;
+
+  return {
+    mode: 'none',
+    target: 'node',
+    entry: {
+      [path.join(packageFolder, packageMainFile)]: path.join(
+        paths.packages,
+        packageFolder,
+        packageMainFile,
+      ),
+    },
+    output: {
+      path: paths.buildCache,
+      filename: '[name]',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              babelrc: true,
+            },
+          },
+        },
+      ],
+    },
+    externals: [nodeExternals()],
+    plugins: [
+      new CopyWebpackPlugin(
+        ['README.md', 'package.json'].map(fileName => ({
+          from: path.join(paths.packages, packageFolder, fileName),
+          to: path.join(paths.buildCache, packageFolder),
+        })),
+      ),
+    ],
+  };
+}
