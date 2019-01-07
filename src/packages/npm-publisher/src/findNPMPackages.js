fca2e3240993a78576c3fe605c1a352def31fa70
Martin Zlámal <mrtnzlml@gmail.com>
2019-01-07 17:02:19 -0600

    Publish NPM-publisher
    kiwicom-source-id: 1c619d50b8f9bc802d6d0e5880820baec204ec4d

---
+++ src/packages/npm-publisher/src/findNPMPackages.js
@@ -0,0 +1,38 @@
+// @flow
+
+import glob from 'glob';
+import path from 'path';
+
+type Callback = ({
+  packageFolderPath: string,
+  packageFolderName: string,
+  packageJSONFile: Object,
+}) => void;
+
+export default function findNPMPackages(
+  packagesLocation: string,
+  cb: Callback,
+) {
+  glob(
+    path.join(packagesLocation, './*/package.json'),
+    (error, packageJSONPaths) => {
+      packageJSONPaths.forEach(packageJSONPath => {
+        // $FlowAllowDynamicImport
+        const packageJSONFile = require(packageJSONPath);
+        // we can publish only public packages
+        if (packageJSONFile.private === false) {
+          const packageFolderPath = path.dirname(packageJSONPath);
+          const packageFolderName = packageFolderPath.replace(
+            new RegExp(packagesLocation + '/'),
+            '',
+          );
+          cb({
+            packageFolderPath,
+            packageFolderName,
+            packageJSONFile,
+          });
+        }
+      });
+    },
+  );
+}
